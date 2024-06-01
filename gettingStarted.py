c713b2b7256021fbb357f204ea164af0a7aab43a
Manny11417 <162273210+Manny11417@users.noreply.github.com>
2024-06-01 19:59:09 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -21,8 +21,7 @@ def welcome_assignment_answers(question):
         answer = 5
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = 3
-    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-        answer = "pcap"
+
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
@@ -38,7 +37,7 @@ if __name__ == "__main__":
     print(welcome_assignment_answers(debug_question))
 
 #Questions:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": “pcap”.
+"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": “pcap”.
 #"Are encoding and encryption the same? - Yes/No":
 #"Is it possible to decrypt a message without a key? - Yes/No":
 #"Is it possible to decode a message without a key? - Yes/No":
