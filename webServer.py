3692cbe33d7e4ab2c0c0c759f5d1e79093d457e6
Manny11417 <162273210+Manny11417@users.noreply.github.com>
2024-06-14 22:34:04 -0400

    Create webServer.py

---
+++ webServer.py
@@ -0,0 +1,60 @@
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
+
+def webServer(port=13331):
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    
+    # Prepare a server socket
+    serverSocket.bind(("", port))
+    serverSocket.listen(1)  # Listening for one connection at a time
+
+    while True:
+        # Establish the connection
+        print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept()
+        
+        try:
+            message = connectionSocket.recv(1024).decode()  # Receiving message from the client
+            filename = message.split()[1]
+            
+            # Open the client requested file
+            f = open(filename[1:], 'rb')
+            
+            # Create a header for a valid response
+            header = "HTTP/1.1 200 OK\r\n"
+            header += "Server: MySimpleWebServer\r\n"
+            header += "Content-Type: text/html; charset=UTF-8\r\n"
+            header += "\r\n"
+            
+            # Send the header
+            connectionSocket.send(header.encode())
+            
+            # Read the file and send its content
+            outputdata = f.read()
+            connectionSocket.send(outputdata)
+            
+            # Close the file
+            f.close()
+            
+            # Close the connection socket
+            connectionSocket.close()
+        
+        except IOError:
+            # Send response message for file not found (404)
+            header = "HTTP/1.1 404 Not Found\r\n"
+            header += "Server: MySimpleWebServer\r\n"
+            header += "Content-Type: text/html; charset=UTF-8\r\n"
+            header += "\r\n"
+            body = "<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1></body></html>"
+            
+            # Send the header and the body
+            connectionSocket.send(header.encode())
+            connectionSocket.send(body.encode())
+            
+            # Close the connection socket
+            connectionSocket.close()
+
+if __name__ == "__main__":
+    webServer(13331)
